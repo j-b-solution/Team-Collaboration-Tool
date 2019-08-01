6516ac877453b2662ba4fd8d37fed65d22f67471
Dongwan Kim <zeumes12@gmail.com>
2019-08-01 17:40:57 -0400

    Added Login Form

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,11 @@
-
 import React, { Component } from 'react';
 import Home from '../components/home/homeComponent';
-
+import Login from '../components/auth/Login';
 class App extends Component {
     render() {
         return (
             <div>
+                
                 <Home />
             </div>
         )
