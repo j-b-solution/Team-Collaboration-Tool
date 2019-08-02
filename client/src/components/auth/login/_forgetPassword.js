fe1daf1a0d14ca2f74edd517a3664616979058b4
Dongwan Kim <zeumes12@gmail.com>
2019-08-02 16:14:42 -0400

    Changed state manage for forgetpw page

--- client/src/components/auth/login/_forgetPassword.js
+++ client/src/components/auth/login/_forgetPassword.js
@@ -2,16 +2,33 @@ import React, { Component } from 'react'
 import Modal from 'react-responsive-modal';
 
 class ForgetPassword extends Component {
+    state = {
+        forgetEmail: ''
+    }
+
+    _btnClear = (event) => {
+        this.setState({
+            forgetEmail: ''
+        })
+    }
+    _onClose = () => {
+        this.props.onClose();
+        this.setState({
+            forgetEmail: ''
+        })
+    }
     render() {
         return (
-            <Modal className="ForgetPW_Modal" open={this.props.open} onClose={this.props.onClose}  center>
+            <Modal className="ForgetPW_Modal" open={this.props.open} onClose={this._onClose}  center>
                 <br />
                 <p className="modal_title">Please enter your email. We will send you instructions to reset your password.</p>
-                <input className="txtbox_email"  text="text" placeholder="enter your email" />
-                <div className="Modal_btnRow">
-                    <button className="Modal_btnClear">Clear</button>
-                    <button className="Modal_btnSubmit">Submit</button>
-                </div>
+                <form>
+                    <input className="txtbox_email"  text="email" name="forgetEmail" placeholder="enter your email" onChange={e => this.setState({[e.target.name]: e.target.value})} />
+                    <div className="Modal_btnRow">
+                        <button type="reset" onClick={this._btnClear} className="Modal_btnClear">Clear</button>
+                        <button className="Modal_btnSubmit" disabled={!this.state.forgetEmail}>Submit</button>
+                    </div>
+                </form>
             </Modal> 
         )
     }
