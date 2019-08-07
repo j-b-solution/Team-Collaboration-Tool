c45944be6f5c2157c7bc3bcf7a594df110211e74
Dongwan Kim <zeumes12@gmail.com>
2019-08-07 19:38:37 -0400

    edited header and home page

--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -1,42 +1,48 @@
 import React, { Component } from 'react';
 import { BrowserRouter as  Route, Link } from "react-router-dom";
 
-import Image_logo from "../../assets/images/logo.png"
+import Image_logo from "../../assets/images/logo_main.png"
 
 
 class Header extends Component {
     render() {
-        return (<nav className="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+        return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light static-top">
             <div className="container">
-                <a className="navbar-brand" href="#">
-                    <img className="img-fluid" src={Image_logo} />
-                </a>
+                <Link className="navbar-brand" to='/'>
+                    <img className="img-fluid logo" src={Image_logo} />
+                </Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarResponsive">
-                    <ul className="navbar-nav ml-auto">
+                    <ul className="navbar-nav ">
                         <li className="nav-item active">
-                            <Link className="nav-link" to='/'>Home
+                            <Link className="nav-link" to='/'>Product
                             <span className="sr-only">(current)</span>
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" href="#">About</Link>
+                            <Link className="nav-link" to="/consult">Contact Sales</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" href="#">Services</Link>
+                            <Link className="nav-link" to="/download">Download</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" href="#">Contact</Link>
+                        <Link className="nav-link" to="/main">Main</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/login">Sign In</Link>
+                            <Link className="nav-link" to="/dashboard">Dashboard</Link>
                         </li>
+                        <li className="nav-item right">
+                            <Link className="nav-link signin" to="/login">Sign In</Link>
+                        </li>
+
                     </ul>
                 </div>
             </div>
-        </nav>);
+        </nav>
+        );
     }
 }
 
