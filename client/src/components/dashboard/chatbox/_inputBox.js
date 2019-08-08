a4aa6ed131bdf181fdcfbf7f6b0a97cbf6ec87b5
Changmin Shin <cshin4@my.centennialcollege.ca>
2019-08-08 19:01:52 -0400

    modified chatbox and inputbox components

--- client/src/components/dashboard/chatbox/_inputBox.js
+++ client/src/components/dashboard/chatbox/_inputBox.js
@@ -1,19 +1,46 @@
 import React from 'react';
+import socketio from 'socket.io-client'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlusCircle } from '@fortawesome/free-solid-svg-icons'
+const socket = socketio.connect('http://localhost:8080')
 
-const InputBox = () => {
-    return (
-        <div >
-            <form className="InputBox_Body">
-                <div className="InputBox_btnPlus">
-                    <FontAwesomeIcon icon={faPlusCircle} size="2x" />
-                </div>
-                <textarea rows="3" cols="130" maxLength='400' placeholder="Send your message..." className="Body_Textbox" type="text"/>
-                <button className="Body_btnSend" type="submit">Send</button>
-            </form>
-        </div>
-    )
+class InputBox extends React.Component  {
+    constructor(props) {
+        super(props)
+        this.state = { name: '', message: '' }
+    }
+    nameChanged(e) {
+        this.setState({ name: e.target.value })
+    }
+    messageChanged(e) {
+        this.setState({ message: e.target.value })
+    }
+   //send name and msg to server
+    send() {
+        socket.emit('chat-msg', {
+            name: this.state.name,
+            message: this.state.message
+        })
+        this.setState({ message: '' }) // initialize msg box after send msg
+    }
+    render() {
+        return (
+            <div>
+                <form className="InputBox_Body">
+                    <div className="InputBox_btnPlus">
+                        <FontAwesomeIcon icon={faPlusCircle} size="2x" />
+                    </div>
+                    <form id="chat">
+                    <input value={this.state.name}
+                    onChange={e => this.nameChanged(e)} placeholder="user name"/><br />
+                    <input value={this.state.message}
+                    onChange={e => this.messageChanged(e)} placeholder="Enter MSG"/><br />
+                        <input type="button" onClick={e => this.send()} class="chat" value="send"/>
+                    </form>                    
+                </form>
+            </div>
+        )
+    }
 }
 
 export default InputBox;
--- client/src/components/dashboard/chatbox/index.js
+++ client/src/components/dashboard/chatbox/index.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import InputBox from './_inputBox';
+import socketio from 'socket.io-client'
+const socket = socketio.connect('http://localhost:8080')
 
 class ChatBox extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            logs: []
+        }
+    }
+    // 컴포넌트가 마운트됐을 때 --- (※5)
+    componentDidMount() {
+        // 실시간으로 로그를 받게 설정
+        socket.on('chat-msg', (obj) => {
+            const logs2 = this.state.logs
+            obj.key = 'key_' + (this.state.logs.length + 1)
+            console.log(obj)
+            logs2.unshift(obj) // 로그에 추가하기
+            this.setState({ logs: logs2 })
+        })
+    }
     render() {
-        const {title, description} = this.props;
+        const messages = this.state.logs.map(e => (
+            <div key={e.key}>
+                <span>{e.name}</span>
+                <span>:{e.message}</span>
+                <p/>
+            </div>
+        ))
+        const { title, description } = this.props;
         return (
             <div className="ChatBox_Container">
                 <div className="ChatBox_titleBox">
@@ -13,6 +39,7 @@ class ChatBox extends Component {
                 </div>
                 <div className="ChatBox_msg">
                     TODO: Message box, bring message history
+                    <div>{messages}</div>
                 </div>
                 <div className="ChatBox_msg_inputBox">
                     <InputBox />
