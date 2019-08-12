345e0139bdfd17c764a71908e63c1158463e687a
Dongwan Kim <zeumes12@gmail.com>
2019-08-12 16:42:05 -0400

    Added footer

--- client/src/components/common/Footer.js
+++ client/src/components/common/Footer.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
+import { BrowserRouter as  Route, Link } from "react-router-dom";
+
+import Image_logo from "../../assets/images/logo_main.png"
 
 class Footer extends Component {
     render(){
-        return<div>this is footer</div>
+        return(
+            <footer className="Footer_container">
+                <div className="Footer_inner">
+                    <div className="Footer_content">
+                        <li className="footer_item">
+                            <img src={Image_logo} alt="logo" />
+                        </li>
+                        <li className="footer_item">
+                            <Link className="footer-link" to="/consult">Contact Sales</Link>
+                        </li>
+                        <li className="footer_item">
+                            <Link className="footer-link" to="/about">About us</Link>
+                        </li>      
+                        <li className="footer_item">
+                            <Link className="footer-link" to="/download">Download</Link>
+                        </li>  
+                        <li className="footer_item">
+                            <Link className="footer-link" to="/consult">SignIn</Link>
+                        </li>
+                    </div>
+
+                </div>
+            </footer>
+        )
     }
 }
 
