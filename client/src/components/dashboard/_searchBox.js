ee11149638c0f20f590a155061b8e230c2a0c42f
Changmin shin <ben@Changmins-MacBook-Pro.local>
2019-08-13 16:48:21 -0400

    modified InputBox user state

--- client/src/components/dashboard/_searchBox.js
+++ client/src/components/dashboard/_searchBox.js
@@ -3,26 +3,29 @@ import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
-import { BrowserRouter as Link} from "react-router-dom";
+
+import { BrowserRouter as Link } from "react-router-dom";
 
 
 const SearchBox = () => {
     return (
-        <div className="SearchBox_container">
+        <section>
+            <div className="SearchBox_container">
                 <button className="SearchBox" alt="prompt search box">
-                <FontAwesomeIcon icon={faSearch} />&nbsp; JUMP TO
+                    <FontAwesomeIcon icon={faSearch} />&nbsp; JUMP TO
                 <span className="tooltiptext">TODO: Need to promp search box</span>
                 </button>
-        </div>
+            </div>
+        </section>
     )
 }
 
 SearchBox.propTypes = {
-    
+
 }
 
 SearchBox.defaultProps = {
-    
+
 }
 
 export default SearchBox;
