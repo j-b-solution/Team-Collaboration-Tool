0acdbb30ab9e545aefc6dfd7e561b19824d44032
Dongwan Kim <zeumes12@gmail.com>
2019-09-08 17:05:17 -0400

    Added the function that keeps chat at the bottom

--- client/src/components/dashboard/chatbox/index.js
+++ client/src/components/dashboard/chatbox/index.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import InputBox from './_inputBox';
 import socketio from 'socket.io-client'
-import { json } from 'body-parser';
+import $ from 'jquery';
 
 const socket = socketio.connect('http://localhost:8080')
 
@@ -31,6 +31,11 @@ class ChatBox extends Component {
                 });
             },
         );
+        this._foucsOnBottom();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        this._foucsOnBottom();
     }
     componentWillMount() {
         // 실시간으로 로그를 받게 설정
@@ -40,8 +45,12 @@ class ChatBox extends Component {
             console.log(obj)
             this.setState({ logs: logs2.concat(obj) })
         })
-    }
 
+    }
+    _foucsOnBottom = () => {
+        const {thing} = this.refs;
+        thing.scrollTop = thing.scrollHeight - thing.clientHeight;
+    }
     _renderHistory = () => {
         let date = '';
         const history = this.state.msg.map((msg, index) => {
@@ -58,9 +67,9 @@ class ChatBox extends Component {
                 <Fragment key={index}>
                     { dateChanged ? <p className="dateSeparator"><span>{date}</span></p> : '' }
                     <div>
-                            <span>{msg.username}</span>
-                            <span> : {msg.message}</span>
-                            <p></p>
+                        <span>{msg.username}</span>
+                        <span> : {msg.message}</span>
+                        <p></p>
                     </div>
                 </Fragment>
         )})
@@ -81,7 +90,6 @@ class ChatBox extends Component {
     }
 
     render() {
-
         const { title, description } = this.props;
         return (
             <div className="ChatBox_Container">
@@ -89,7 +97,7 @@ class ChatBox extends Component {
                     <p className="ChatBox_title">{title}</p>
                     <p className="ChatBox_desc">{description}</p>
                 </div>
-                <div className="ChatBox_msg">
+                <div ref={`thing`} className="ChatBox_msg">
                     <div>{this._renderHistory()}</div>
                     <div>{this._renderLiveMessage()}</div>
                 </div>
