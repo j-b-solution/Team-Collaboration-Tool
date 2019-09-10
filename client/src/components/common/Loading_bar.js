1dc193a1fc5283acd8d72220c3476731e0e7e7ba
Dongwan Kim <zeumes12@gmail.com>
2019-09-09 20:46:34 -0400

    Added loading bar on message history

--- client/src/components/common/Loading_bar.js
+++ client/src/components/common/Loading_bar.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactLoading from 'react-loading';
 
-const LoadingBar = ({ type, color }) => (
-    <div className="Loading__Bar">
-        <ReactLoading  type={type} color={color}  />
-    </div>
+const LoadingBar = ({ type, color, msg }) => (
+    <Fragment>
+        <div className="Loading__Bar">
+            <ReactLoading  type={type} color={color}  />
+        </div>
+        <p className="Loading__Message">{msg}</p>
+    </Fragment>
 );
 
 export default LoadingBar;
