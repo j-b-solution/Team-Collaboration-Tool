1ee350d20eeb8dacea9810e4d0a24d4fe13db12c
Dongwan Kim <zeumes12@gmail.com>
2019-09-15 22:08:10 -0400

    added build to the project

---
+++ client/build/precache-manifest.a22014830d3c2d64d2683fa81215451f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d893085f7ebc559cff4baf6101de30b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e022cedd15eca796561",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "e30da8cd9bec5edb9695",
+    "url": "/static/css/main.987fe79a.chunk.css"
+  },
+  {
+    "revision": "2e022cedd15eca796561",
+    "url": "/static/js/2.ebaaf425.chunk.js"
+  },
+  {
+    "revision": "e30da8cd9bec5edb9695",
+    "url": "/static/js/main.876174e4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "768c3e91eaeb1cd8474282225ba1ee4b",
+    "url": "/static/media/clipart350383.768c3e91.png"
+  },
+  {
+    "revision": "ee7ebc5b5214e6fa2cb163471d8766ae",
+    "url": "/static/media/home_1.ee7ebc5b.png"
+  },
+  {
+    "revision": "cf09547b698c554cfbb48429437d2155",
+    "url": "/static/media/home_2.cf09547b.png"
+  },
+  {
+    "revision": "6c1932d84c33e8e189ded188667e1298",
+    "url": "/static/media/logo_main.6c1932d8.png"
+  }
+]);
