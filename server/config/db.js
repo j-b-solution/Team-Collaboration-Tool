8f9e93cc6e2ef9c2f8ebcdbf851eee03d30c3ff9
Dongwan Kim <zeumes12@gmail.com>
2019-09-15 21:04:21 -0400

    Changed db to cloud db

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
-    "URI" : "mongodb://localhost/test",
+    // "URI" : "mongodb://localhost/test",
+    "URI": "mongodb+srv://dongwan:a123456789@cluster0-legcd.mongodb.net/test?retryWrites=true&w=majority",
     "secret": "Somesecret"
 }
